b50214dead19e01c9156d69ee601a61b1320adb1
t-h-e-r-o-x <31180828+t-h-e-r-o-x@users.noreply.github.com>
2019-01-13 16:42:57 +0530

    DynamicPortAddress

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res ,db)})
 app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App is running on port ${process.env.PORT}');
 });
